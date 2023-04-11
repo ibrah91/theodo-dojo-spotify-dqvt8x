d5d4500a92ac909b09da000e6f18011c299d99c7
ibrah91 <47572340+ibrah91@users.noreply.github.com>
2023-04-11 19:39:26 +0200

    Tracklist

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import logo from './assets/logo.svg';
 import './App.css';
 
 const App = () => {
+  const trackUrls = [
+    'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
+    'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
+    'https://p.scdn.co/mp3-preview/31f65b6a613010f22316c7be335b62226cf2f263',
+    'https://p.scdn.co/mp3-preview/0f6b8a3524ec410020457da4cdd7717f9addce2f',
+    'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
+  ];
   return (
     <div className="App">
       <header className="App-header">
@@ -9,7 +16,9 @@ const App = () => {
         <h1 className="App-title">Bienvenue sur le blind test</h1>
       </header>
       <div className="App-images">
-        <p>hello maxime</p>
+      <audio src={trackUrls[4]} autoPlay controls/>
+
+        
       </div>
       <div className="App-buttons"></div>
     </div>
